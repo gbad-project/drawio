388c42475105f2aa2fe2783caa304a407b66ba7d
Pavel Zhelnov <pzhelnov@p1m.org>
2025-10-23 13:43:19 -0400

    skip tests in run_regeneration.sh and add verbosity to pytest in test_legacy.sh
    this prevents double testing and shows everything nice
    in pytest under test_legacy, including - to come! - xfails

--- src/main/webapp/plugins/rdfexport/scripts/test_legacy.sh
+++ src/main/webapp/plugins/rdfexport/scripts/test_legacy.sh
@@ -20,4 +20,4 @@ bun run build:py
 # Produce reliable, original draw io parser output artifacts
 cd ../../../../.. && bash src/main/webapp/plugins/rdfexport/legacy/scripts/run_regeneration.sh
 # Test draw io parser patches applied for rdfexport plugin integration
-cd src/main/webapp/plugins/rdfexport && pytest legacy/tests/ meta_builder/tests/
+cd src/main/webapp/plugins/rdfexport && pytest -rA legacy/tests/ meta_builder/tests/
