0d4636f4cc4c27fdb7aa8efc6d25817f86da564b
Pavel Zhelnov <pzhelnov@p1m.org>
2025-11-20 17:11:36 -0500

    add base extraction from turtle with a test
    this fully passes now:
    
    pytest -s -vv python_core/tests/test_draw_io_parser_graph.py

--- src/main/webapp/plugins/rdfexport/python_core/src/overrides/core/rdf/control/draw_io_parser_graph.py
+++ src/main/webapp/plugins/rdfexport/python_core/src/overrides/core/rdf/control/draw_io_parser_graph.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+from io import StringIO
+
 from python_core.src.draw_io_parser import *  # type: ignore=imported-unused
 from aicode.python_core.meta_builder.drawio_meta_builder import override
 
 from rdflib.term import Node
+from rdflib import Graph
+from rdflib.parser import InputSource, create_input_source
+from rdflib.plugins.parsers.notation3 import RDFSink, SinkParser
 
 # ruff: noqa: F403, F405
 
@@ -35,3 +40,70 @@ class DrawIOParserGraph(Graph):
         """
         self.add(triple_1)
         self.addN((s, p, o, self) for s, p, o in triples_N)
+
+    @staticmethod
+    def _extract_base_from_inputsource(source: InputSource):
+        """
+        Reuses some code from `TurtleParser.parse()`:
+        https://rdflib.readthedocs.io/en/7.1.1/_modules/rdflib/plugins/parsers/notation3.html#TurtleParser
+        """
+        graph = Graph()
+        sink = RDFSink(graph)
+        baseURI = graph.absolutize(source.getPublicId() or source.getSystemId() or "")
+        p = SinkParser(
+            sink,
+            baseURI=graph.absolutize(
+                source.getPublicId() or source.getSystemId() or ""
+            ),
+            turtle=True,
+        )
+        stream = source.getCharacterStream()
+        if not stream:
+            stream = source.getByteStream()
+        p.loadStream(stream)
+        baseURI = p._baseURI
+        return baseURI
+
+    @classmethod
+    def extract_base_from_turtle(cls, turtle_str: str) -> str:
+        """
+        Parse the given Turtle document (string) and return
+        the base URI used during parsing.
+
+        The need for this function is dictated by the fact that
+        rdflib seems NOT to read @base with `TurtleParser`
+        into `base` property but rather resolves IRIs
+        using base upon serialization. This leads to problems
+        if graph is serialized and then parsed again repeatedly
+        (i.e., `@base` is lost on repeat serialization).
+
+        <https://stackoverflow.com/questions/43739259/how-do-i-get-the-base-uri-of-an-xml-file-using-rdflib>
+        """
+        source = InputSource()
+        source.setCharacterStream(StringIO(turtle_str))
+        return cls._extract_base_from_inputsource(source)
+
+    def _inject_base_from_parse_kwargs(self, **kwargs):
+        if self.base:
+            return
+        self.base = self._extract_base_from_inputsource(
+            create_input_source(
+                source=kwargs.get("source"),
+                publicID=kwargs.get("publicID"),
+                location=kwargs.get("location"),
+                file=kwargs.get("file"),
+                data=kwargs.get("data"),
+                format=kwargs.get("format"),
+            )
+        )
+
+    def parse(self, *args, **kwargs):
+        """
+        Same as in `super()` but injects `base` from Turtle
+        if not already set and if available, thus making
+        it persist across reserialiation/parsing cycles.
+
+        Also see docstring for `extract_base_from_turtle()`.
+        """
+        super().parse(*args, **kwargs)
+        self._inject_base_from_parse_kwargs(**kwargs)
---
+++ src/main/webapp/plugins/rdfexport/python_core/tests/test_draw_io_parser_graph.py
@@ -0,0 +1,49 @@
+import pytest
+
+from rdflib import URIRef, Graph
+
+from python_core.src.overrides.core.rdf.control.draw_io_parser_graph import (
+    DrawIOParserGraph,
+)
+
+
+class SampleTurtleWithBase:
+    def __init__(self):
+        self.base = URIRef("http://mock_test_extract_base_from_turtle")
+        self.turtle = """@base <{}> .
+</mock_subject> a <#mock_object> .
+""".format(self.base)
+
+
+@pytest.fixture
+def fixture():
+    return SampleTurtleWithBase()
+
+
+def test_extract_base_from_turtle(fixture):
+    extracted_base = DrawIOParserGraph.extract_base_from_turtle(fixture.turtle)
+    assert extracted_base == fixture.base
+
+
+def test_base_extraction_on_parse(fixture):
+    # First, let's confirm that `base` is not set
+    # when parsing a Turtle string with a known
+    # @base directive to a regular Graph
+    g = Graph()
+    g.parse(data=fixture.turtle, format="turtle")
+    assert g.base is None  # because IRIs are resolved
+
+    # Now, DrawIOParserGraph:
+    # parse and serialize
+    dpg1 = DrawIOParserGraph()
+    dpg1.parse(data=fixture.turtle, format="turtle")
+    assert dpg1.base == fixture.base
+
+    # The next part is strictly not
+    # necessary because the assert above
+    # already checks out, but let's confirm:
+    serialized = dpg1.serialize()
+    # parse again
+    dpg2 = DrawIOParserGraph()
+    dpg2.parse(data=serialized, format="turtle")
+    assert dpg2.base == fixture.base
