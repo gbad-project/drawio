ebf31f9be0cbb68c48782195fbba50597cfec8bb
Pavel Zhelnov <pzhelnov@p1m.org>
2025-12-09 12:34:01 -0500

    confirm test all log still no fails
    now need to test on prod
    and play with default yml values

--- src/main/webapp/plugins/rdfexport/python_core/src/draw_io_parser.py
+++ src/main/webapp/plugins/rdfexport/python_core/src/draw_io_parser.py
@@ -15,20 +15,21 @@ import traceback
 import os
 from rdflib import Graph, URIRef, Literal, Namespace
 from rdflib.namespace import RDF, RDFS, OWL, XSD
+from enum import Enum, auto
 import json
 from html import unescape
-from enum import Enum, auto
-from typing import Callable
-from rdflib import BNode, SKOS
-from rdflib.collection import Collection
 import re
+from rdflib import BNode
 from rdflib.term import Node
-from rdflib.namespace import NamespaceManager
+from typing import Callable
+from rdflib import SKOS
+from rdflib.collection import Collection
+import typing
+import logging
 from io import StringIO
 from rdflib.parser import InputSource, create_input_source
 from rdflib.plugins.parsers.notation3 import RDFSink, SinkParser
-import typing
-import logging
+from rdflib.namespace import NamespaceManager
 
 
 class pipeline:
